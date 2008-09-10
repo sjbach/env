dca82c04027b914d822141137606f52371fd6151
Stephen Bach <github@sjbach.com>
2008-09-10 17:11:45 -0400

    Fix stdin as input

--- bin/tim.sh
+++ bin/tim.sh
@@ -4,12 +4,12 @@
 VIM_PARAMS="+set nowrap buftype=nofile bufhidden=hide"
 
 if [ "$DISPLAY" ]; then
-  (
+  cat 2>/dev/null | (
     if [ "$1" ] && [ ! "$1" == "-" ]; then
       title_string=`echo "$@" | sed 's/ /\\\\ /g'`
       "$@" | gvim "$VIM_PARAMS titlestring=$title_string" -
     else
-      exec gvim "$VIM_PARAMS titlestring=stdin" -
+      exec gvim "$VIM_PARAMS titlestring=<stdin>" -
     fi
   ) >/dev/null &
 else
@@ -17,7 +17,7 @@ else
     title_string=`echo "$@" | sed 's/ /\\\\ /g'`
     "$@" | vim "$VIM_PARAMS titlestring=$title_string" -
   else
-    exec vim "$VIM_PARAMS titlestring=stdin" -
+    exec vim "$VIM_PARAMS titlestring=<stdin>" -
   fi
 fi
 
