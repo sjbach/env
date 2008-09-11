e11897c5a13a4e4c95c22021fc62bd7cab62bd9a
Stephen Bach <github@sjbach.com>
2008-09-10 23:29:07 -0400

    Avoid execing nothing

--- bin/vim.sh
+++ bin/vim.sh
@@ -3,8 +3,11 @@
 # When on an ssh session with X forwarding, sometimes connecting to the X
 # display takes a long time and there's just no reason to even try.
 
-# (Avoid infinite recursion.)
+# Avoid infinite recursion.
 VIM=`which -a vim | sed 1d | head -n1`
+
+# (Just in case this script isn't in the path.)
+[ "$VIM" ] || VIM=vim
 
 DISPLAY= exec $VIM "$@"
 
