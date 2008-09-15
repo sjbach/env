758cb9bd0283bcacfab035bc53defacec2104e4d
Stephen Bach <github@sjbach.com>
2008-09-14 22:35:11 -0400

    Call recursively for multiple words.

--- bin/greek.bash
+++ bin/greek.bash
@@ -2,24 +2,28 @@
 #
 # Download the pronunciations for the given word and play them.
 
-
-if [ $# -ne 1 ]; then
-	echo "Incorrect arguments." >&2
-	exit 2
-fi
+case $# in
+  0) echo "Error: No words given." >&2
+     exit 2
+     ;;
+  1) ;;
+  *) for word in $@; do
+       $0 "$word"
+     done
+     exit
+     ;;
+esac
 
 SOUND_DIR=~/.greek
-NEWLINE='\
-'
 
 [ ! -d $SOUND_DIR ] && mkdir -p $SOUND_DIR
 
 WAVS=$(\
   wget -q -O - "http://www.m-w.com/dictionary/$1" | \
-  sed -n "/audio\.gif/s/>/$NEWLINE/pg" | \
+  sed -n '/audio\.gif/s/>/\n/pg' | \
   sed -rn "/popWin/s|^.*popWin\('([^']*)'\).*$|http://www\.m-w\.com/\1|gp"| \
   xargs -r wget -q -O - | \
-  sed -n '/Click here/s/^.*<A HREF="\([^"]*\)">.*$/\1/gp')
+  sed -rn '/Click here/s/^.*<A HREF="([^"]*)">.*$/\1/gp')
 
 if [ -z "$WAVS" ]; then
 	echo "No sounds found for \"$1\"." >&2
