2e6e97f05903401bcfecb892097d683705cf6ec0
Stephen Bach <git@sjbach.com>
2008-11-01 17:10:02 -0400

    Make compliant wit sh

--- bin/tim.sh
+++ bin/tim.sh
@@ -5,7 +5,7 @@ VIM_PARAMS="+set nowrap buftype=nofile bufhidden=hide"
 
 if [ "$DISPLAY" ]; then
   cat 2>/dev/null | (
-    if [ "$1" ] && [ ! "$1" == "-" ]; then
+    if [ "$1" ] && [ ! "$1" = "-" ]; then
       title_string=`echo "$@" | sed 's/ /\\\\ /g'`
       "$@" | gvim "$VIM_PARAMS titlestring=$title_string" -
     else
@@ -13,7 +13,7 @@ if [ "$DISPLAY" ]; then
     fi
   ) >/dev/null &
 else
-  if [ "$1" ] && [ ! "$1" == "-" ]; then
+  if [ "$1" ] && [ ! "$1" = "-" ]; then
     title_string=`echo "$@" | sed 's/ /\\\\ /g'`
     "$@" | vim "$VIM_PARAMS titlestring=$title_string" -
   else
