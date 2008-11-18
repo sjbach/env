2eb7775ab6a846fe1920518114452d15df18f0b1
Stephen Bach <github@sjbach.com>
2008-11-18 17:38:46 -0500

    Start w3m on the bookmarks page

---
+++ bin/w3m.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Start w3m on the bookmark page by default.
+
+# Avoid infinite recursion.
+W3M=`which -a w3m | sed 1d | head -n1`
+[ "$W3M" ] || W3M=`which vim`
+[ "$W3M" ] || W3M=w3m
+
+[ "$@" ] || exec $W3M -B
+exec $W3M "$@"
+
