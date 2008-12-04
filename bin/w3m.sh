da851d53ce62461d9c5818db7590ab5b373681b8
Stephen Bach <github@sjbach.com>
2008-12-04 15:55:20 -0500

    Whoops

--- bin/w3m.sh
+++ bin/w3m.sh
@@ -4,7 +4,7 @@
 
 # Avoid infinite recursion.
 W3M=`which -a w3m | sed 1d | head -n1`
-[ "$W3M" ] || W3M=`which vim`
+[ "$W3M" ] || W3M=`which w3m`
 [ "$W3M" ] || W3M=w3m
 
 [ "$@" ] || exec $W3M -B
