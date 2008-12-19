c4c2473261deb6f2bba3dceb3515e66ee8c51b7d
Stephen Bach <github@sjbach.com>
2008-12-18 21:30:19 -0500

    Better (and slower) strategy for recency ordering

--- bin/histgrep.bash
+++ bin/histgrep.bash
@@ -1,10 +1,28 @@
 #!/bin/bash
+#
 # Grep through archived command history.
+#
 
-# Get results
-grep "$@" -- ~/.histfile ~/.hists/* | \
+# Get results, reverse, prepend line numbers
+grep "$@" -- ~/.histfile ~/.hists/* | tac | nl | \
+#
+# Insert line numbers for better intra-day sorting
+sed -r 's/([0-9]+) *([^:]*):(.*)/\2:\1:\3/' | \
+#
 # Pretty print
-sed -r 's/.*\.hists\/(.*).(histfile|bash_history):/\1:/' | \
+sed -r 's%.*\.hists/(.*).(histfile|bash_history):%\1:%' | \
+#
+# Put more recent lines first like this:
+#   year, month, day, then implicitly by the inserted line number
+# Do this now so that the unique sort prefers more recent lines.
+sort --key 1.7,1.8nr --key 1.1,1.3Mr --key 1.4,1.5nr | \
+#
 # Remove duplicates
-sort -u -t ':' --key 2 | sort -r -M
+sort --stable -u -t ':' --key 3 | \
+#
+# Put more recent lines first again (it gets screwed up)
+sort --key 1.7,1.8nr --key 1.1,1.3Mr --key 1.4,1.5nr | \
+#
+# Remove the inserted line numbers.
+sed -r 's/:[0-9]+:/:/'
 
