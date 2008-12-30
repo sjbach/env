9de3261ada562e80072e5222b1f4f8adcaecfe3b
Stephen Bach <git@sjbach.com>
2008-12-30 09:32:43 -0500

    Check for bookmark file

--- bin/w3m.sh
+++ bin/w3m.sh
@@ -7,6 +7,9 @@ W3M=`which -a w3m | sed 1d | head -n1`
 [ "$W3M" ] || W3M=`which w3m`
 [ "$W3M" ] || W3M=w3m
 
-[ "$@" ] || exec $W3M -B
-exec $W3M "$@"
+if [ "$@" ] || [ ! -e ~/.w3m/bookmark.html ]; then
+  exec $W3M "$@"
+else
+  exec $W3M -B
+fi
 
