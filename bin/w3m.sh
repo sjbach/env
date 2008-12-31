40aa96975529f024d3bfe5101e69f59201f118c2
Stephen Bach <git@sjbach.com>
2008-12-31 09:00:25 -0500

    Fix for multple arguments

--- bin/w3m.sh
+++ bin/w3m.sh
@@ -7,7 +7,7 @@ W3M=`which -a w3m | sed 1d | head -n1`
 [ "$W3M" ] || W3M=`which w3m`
 [ "$W3M" ] || W3M=w3m
 
-if [ "$@" ] || [ ! -e ~/.w3m/bookmark.html ]; then
+if [ "$1" ] || [ ! -e ~/.w3m/bookmark.html ]; then
   exec $W3M "$@"
 else
   exec $W3M -B
