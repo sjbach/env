2f78ff1243307658a70a7025024deddbecd5b64f
Stephen Bach <github@sjbach.com>
2009-01-16 15:09:36 -0500

    Clean up

--- bin/dim.sh
+++ bin/dim.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# Send stdin or output from running arguments to a temporary gvim buffer as
+# Send stdin or output from executing arguments to a temporary gvim buffer as
 # well as stdout.
 
 [ "$DISPLAY" ] || exec tim "$@"
 
-if ! which pee >/dev/null 2>&1; then 
-  echo "Install package 'moreutils'" >&2 
+if ! which pee >/dev/null 2>&1 ; then
+  echo "Install package 'moreutils'" >&2
   exit 1
 fi
 
-GVIM_PARAMS="+set nowrap buftype=nofile bufhidden=hide"
+params="+set nowrap buftype=nofile bufhidden=hide"
 
 if [ "$1" ] && [ "$1" != "-" ]; then
-  TITLE=`echo "$@" | sed 's/ /\\\\ /g'`
-  "$@" | pee "gvim \"$GVIM_PARAMS titlestring=$TITLE\" - 2>/dev/null" \
+  title=`echo "$@" | sed 's/ /\\\\ /g'`
+  "$@" | pee "gvim \"$params titlestring=$title\" - 2>/dev/null" \
              cat
 else
-  TITLE='<stdin>'
-  exec pee "gvim \"$GVIM_PARAMS titlestring=$TITLE\" - 2>/dev/null" \
+  title='<stdin>'
+  exec pee "gvim \"$params titlestring=$title\" - 2>/dev/null" \
            cat
 fi
 
