f365e836bb8e6b999dff0de87b805a4fb6298085
Stephen Bach <github@sjbach.com>
2009-01-23 17:13:28 -0500

    Default to search if args are not a file or URL.

--- bin/w3m.sh
+++ bin/w3m.sh
@@ -7,8 +7,16 @@ W3M=`which -a w3m | sed 1d | head -n1`
 [ "$W3M" ] || W3M=`which w3m`
 [ "$W3M" ] || W3M=w3m
 
-if [ "$1" ] || [ ! -e ~/.w3m/bookmark.html ]; then
-  exec $W3M "$@"
+if [ "$1" ]; then
+  if [ -e "$1" ] || echo "$*" | grep -q '\.' ; then
+    exec $W3M "$@"
+  else
+    terms=`echo "$*" | sed 's/ /+/g'`
+    url="http://www.google.com/search?q=$terms"
+    exec $W3M "$url"
+  fi
+elif [ ! -e ~/.w3m/bookmark.html ]; then
+  exec $W3M
 else
   exec $W3M -B
 fi
