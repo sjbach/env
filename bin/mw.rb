1c52dbbd8e11c2a18ec4e0246d35e2ed9b149f36
Stephen Bach <github@sjbach.com>
2016-02-23 17:30:35 -0800

    mw: better parsing and printing of full definition lists

--- bin/mw.rb
+++ bin/mw.rb
@@ -13,11 +13,20 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/http'
 
+#require 'pp'
 $debug = false
 
 if ARGV[0].nil?
   $stderr.puts "Usage: m-w <word>"
   exit 0
+end
+
+class String
+  # Hack: String#strip doesn't remove non-breaking space A0 by default, so
+  # pre-convert them to ' '.
+  def strip_nbsp
+    return self.gsub("\u00A0", " ").strip
+  end
 end
 
 def main
@@ -38,13 +47,13 @@ def main
     if classes.include?('examples-box')
       puts 'Examples...'
       card_box.css('.definition-list li').each do |e|
-        wrapped = wrap_text("#{e.inner_text.strip}", "   ")
+        wrapped = wrap_text("#{e.inner_text.strip_nbsp}", "   ")
         puts wrapped.sub(/^  /," -")
       end
     elsif classes.include?('origin-box')
       puts 'Origin...'
       card_box.css('div.card-primary-content p').each do |p|
-        puts wrap_text(p.inner_text.strip, " ")
+        puts wrap_text(p.inner_text.strip_nbsp, " ")
       end
     elsif classes.include?('rhymes-with-box')
       # (Elided.)
@@ -52,7 +61,7 @@ def main
       # TODO put all on one line?
       puts 'First known use...'
       card_box.css('div.card-primary-content p').each do |p|
-        puts wrap_text(p.inner_text.strip, " ")
+        puts wrap_text(p.inner_text.strip_nbsp, " ")
       end
     elsif classes.include?('related-box')
       puts 'Related...'
@@ -60,19 +69,19 @@ def main
     elsif classes.include?('other-x-terms-box')
       puts 'Other field terms...'
       card_box.css('div.card-primary-content p').each do |p|
-        puts wrap_text(p.inner_text.strip, " ")
+        puts wrap_text(p.inner_text.strip_nbsp, " ")
       end
     elsif classes.include?('related-phrases-box')
       puts 'Related phrases...'
       card_box.css('div.card-primary-content li').each do |li|
-        wrapped = wrap_text("#{li.inner_text.strip}", "   ")
+        wrapped = wrap_text("#{li.inner_text.strip_nbsp}", "   ")
         puts wrapped.sub(/^  /," -")
       end
 
     elsif classes.include?('synonym-discussion-box')
       puts 'Synonym discussion...'
       card_box.css('div.card-primary-content .definition-block').each do |li|
-        puts wrap_text(li.inner_text.strip, " ")
+        puts wrap_text(li.inner_text.strip_nbsp, " ")
       end
 
     elsif classes.include?('quick-def-box')
@@ -83,20 +92,7 @@ def main
 
     elsif classes.include?('full-def-box')
       puts if just_parsed_full_def or not just_parsed_quick_def
-
-      term = card_box.at_css('h1, h2 i') or card_box.at_css('h1, h2')
-      # TODO double check this ever occurs
-      function = card_box.at_css('.word-attributes .main-attr')
-      pronunciation = card_box.at_css('.word-attributes .pr')
-      puts "Full: #{term.inner_text.strip}" if term
-      puts "Function: #{function.inner_text.strip}" if function
-      puts "Pronunciation: #{pronunciation.inner_text.strip}" if pronunciation
-
-      card_box.css('.definition-list li').each do |e|
-        wrapped = wrap_text("#{e.inner_text.strip}", "   ")
-        puts wrapped.sub(/^  /," :")
-      end
-
+      parse_and_print_full_def_box(card_box)
       just_parsed_quick_def = false
       just_parsed_full_def = true
 
@@ -192,6 +188,22 @@ def uri_escape(str)
                  .gsub("]", "%5D")
 end
 
+def node_has_class(node, str_or_array)
+  classes = node.attributes['class'].to_s.split
+  case str_or_array
+  when String
+    return classes.include?(str_or_array)
+  when Array
+    return (classes & str_or_array).any?
+  else
+    assert(false, "bad class: #{str_or_array.class}")
+  end
+end
+
+def node_is_nonstandard_intro_colon(node)
+  return (node.name == 'strong' and node.inner_text.strip_nbsp == ':')
+end
+
 def parse_and_print_synonym_box(card_box_node)
   heading = nil
   text = ''
@@ -200,16 +212,16 @@ def parse_and_print_synonym_box(card_box_node)
     case node.name
     when 'h6'
       unless heading.nil?
-        puts wrap_text(text.strip, "   ")
+        puts wrap_text(text.strip_nbsp, "   ")
       end
-      heading = node.inner_text.strip
+      heading = node.inner_text.strip_nbsp
       puts wrap_text("#{heading}:", " ")
       text = ''
     else  # In practice, 'text', 'a', and 'em'.
       text += node.inner_text
     end
   end
-  puts wrap_text(text.strip, "   ")
+  puts wrap_text(text.strip_nbsp, "   ")
 end
 
 
@@ -217,9 +229,9 @@ def parse_and_print_quick_def_box(card_box_node)
   term = card_box_node.at_css('h1, h2 i') or card_box_node.at_css('h1, h2')
   function = card_box_node.at_css('.word-attributes .main-attr')
   pronunciation = card_box_node.at_css('.word-attributes .pr')
-  puts "Quick: #{term.inner_text.strip}" if term
-  puts "Function: #{function.inner_text.strip}" if function
-  puts "Pronunciation: #{pronunciation.inner_text.strip}" if pronunciation
+  puts "Quick: #{term.inner_text.strip_nbsp}" if term
+  puts "Function: #{function.inner_text.strip_nbsp}" if function
+  puts "Pronunciation: #{pronunciation.inner_text.strip_nbsp}" if pronunciation
 
   card_box_node.css('.definition-list .definition-inner-item > span').each \
   do |outer_span|
@@ -227,17 +239,125 @@ def parse_and_print_quick_def_box(card_box_node)
     text = ''
     outer_span.children.each do |node|
       assert(node.element? || node.text?)
-      if node.attributes['class'].to_s =~ /intro-colon/
-        prefix += "#{node.inner_text.strip} "
+      if node_has_class(node, 'intro-colon')
+        prefix += "#{node.inner_text.strip_nbsp} "
       else
         text += node.inner_text
       end
     end
 
-    wrapped = wrap_text("#{text.strip}", " " + " " * prefix.length)
+    wrapped = wrap_text("#{text.strip_nbsp}", " " + " " * prefix.length)
     wrapped[0..(1 + prefix.length - 1)] = " #{prefix}"
     puts wrapped
   end
+end
+
+def parse_and_print_full_def_box(card_box_node)
+  # TODO inflections
+
+  term = card_box_node.at_css('h1, h2 i') or card_box_node.at_css('h1, h2')
+  # TODO double check this ever occurs
+  function = card_box_node.at_css('.word-attributes .main-attr')
+  pronunciation = card_box_node.at_css('.word-attributes .pr')
+  puts "Full: #{term.inner_text.strip_nbsp}" if term
+  puts "Function: #{function.inner_text.strip_nbsp}" if function
+  puts "Pronunciation: #{pronunciation.inner_text.strip_nbsp}" if pronunciation
+
+  card_box_node.css('.card-primary-content') do |primary_node|
+    # TODO loop over this first, consuming dro / runon-attributes
+  end
+
+  card_box_node.css('.definition-list > li').each do |li|
+    if node_has_class(li, 'vt')
+      puts " /#{li.inner_text.strip_nbsp}/"
+    else
+      prev_def_item = nil
+      def_item = DefItem.new
+
+      li.at_css('> p').children.each do |node|
+        if (node_has_class(node, ['sense', 'sub', 'intro-colon']) or
+            node_is_nonstandard_intro_colon(node))
+          def_item.incorporate(node)
+        elsif node.name == 'span'
+          # Recurse another level.
+          node.children.each do |node_inner|
+            if (node_has_class(node_inner, ['sense', 'sub', 'intro-colon']) or
+                node_is_nonstandard_intro_colon(node_inner))
+              if def_item.appears_complete?
+                print_def_item(def_item, prev_def_item)
+                prev_def_item = def_item.clone
+                def_item.text = ''
+              end
+              def_item.incorporate(node_inner)
+            else
+              def_item.incorporate(node_inner)
+            end
+          end
+          # Last entry in sub-list.
+          assert(def_item.appears_complete?)
+          print_def_item(def_item, prev_def_item)
+          prev_def_item = def_item.clone
+          def_item.text = ''
+        else  # text
+          def_item.incorporate(node)
+        end
+      end
+
+      # Last entry.
+      if def_item.appears_complete?
+        print_def_item(def_item, prev_def_item)
+      end
+    end
+  end
+end
+
+class DefItem
+  attr_accessor :sense_num, :sub_alpha, :colon, :text
+
+  def initialize
+    @text = ''
+  end
+
+  def incorporate(node)
+    if node_has_class(node, 'sub')
+      @sub_alpha = node.inner_text.strip_nbsp
+    elsif node_has_class(node, 'sense')
+      @sense_num = node.inner_text.strip_nbsp
+    elsif (node_has_class(node, 'intro-colon') or 
+           node_is_nonstandard_intro_colon(node))
+      @colon = node.inner_text.strip_nbsp
+      assert(@colon == ':')
+    else
+      @text += node.inner_text
+    end
+  end
+
+  def appears_complete?
+    # Some definitions do not include an intro-colon, or we would check that
+    # here as well.
+    return !(@text.nil? or @text.empty? or @text.strip_nbsp.empty?)
+    #return !(@colon.nil? or @colon.empty? or
+    #         @text.nil? or @text.empty? or @text.strip_nbsp.empty?)
+  end
+end
+
+def print_def_item(def_item, prev_def_item)
+  prefix = [def_item.sense_num,
+            def_item.sub_alpha,
+            def_item.colon]
+  if prev_def_item
+    if def_item.sense_num and (def_item.sense_num == prev_def_item.sense_num)
+      prefix[0] = ' '
+    end
+    if def_item.sub_alpha and (def_item.sub_alpha == prev_def_item.sub_alpha)
+      prefix[1] = ' '
+    end
+  end
+  prefix_str = prefix.compact.join(' ') + ' '
+  wrapped = wrap_text("#{def_item.text.strip_nbsp}",
+                      " " + " " * prefix_str.length)
+  wrapped[0..(1 + prefix_str.length - 1)] = " #{prefix_str}"
+  puts wrapped
 end
 
 main()
