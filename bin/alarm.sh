eac90834c77025409eaf40edf23f55bc8b3fab10
Stephen Bach <github@sjbach.com>
2016-07-16 15:46:40 -0700

    alarm: print an on-screen message

--- bin/alarm.sh
+++ bin/alarm.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 # Three beeps
 
+msg="$1"
+if [ -z "$msg" ]; then
+  msg="DONE"
+fi
+
 (
+  if [ "$DISPLAY" ]; then
+    osdcat "$msg"
+  fi
   for i in 1 2 3; do
     /bin/echo -en '\a'
-    sleep 0.3 
+    sleep 0.3
   done 2>/dev/null
 ) &
 
