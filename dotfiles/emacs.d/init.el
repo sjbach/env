624358d75d714a6b35ce236d8f498707ab14621b
Stephen Bach <github@sjbach.com>
2019-11-10 15:31:51 -0500

    Emacs: save and restore *scratch* across sessions
    Some hijinks required to make this work, contrary to the `remember`
    documentation which suggests it is straightforward.

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -25,9 +25,29 @@
 ;; Save bookmarks to the fileysystem whenever there are changes.
 (setq bookmark-save-flag 1)
 
-;; Don't let *scratch* be closed accidentally.
-(with-current-buffer "*scratch*"
-  (emacs-lock-mode 'kill))
+;; Save/restore *scratch* across sessions.
+;;
+;; The documentation for remember-notes says: "Set ‘initial-buffer-choice’ to
+;; ‘remember-notes’ to visit your notes buffer when Emacs starts.  Set
+;; ‘remember-notes-buffer-name’ to "*scratch*" to turn the *scratch* buffer
+;; into your notes buffer."
+;;
+;; This appears to be incorrect - if *scratch* exists when remember-notes is
+;; called (and it always will exist), then the notes buffer won't be renamed to
+;; *scratch*. To work around this, we have to kill the *scratch* buffer before
+;; remember-notes is called. Emacs automatically recreates the *scratch* buffer
+;; with the correct modes, so not a big deal, but hacky.
+(require 'remember)
+(setq remember-notes-buffer-name "*scratch*")
+;; (setq initial-buffer-choice #'remember-notes)
+(setq initial-buffer-choice
+      (lambda ()
+        (kill-buffer remember-notes-buffer-name)  ; *scratch*
+        (prog1
+            (remember-notes)
+          ;; Don't let *scratch* be closed accidentally.
+          (with-current-buffer "*scratch*"
+            (emacs-lock-mode 'kill)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
