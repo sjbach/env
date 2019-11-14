cda14c6f70322d33dc73a9c76d577bbfec63e56f
Stephen Bach <github@sjbach.com>
2019-11-14 10:24:44 -0500

    Move custom/customize stuff into its own file

---
+++ dotfiles/emacs.d/custom.el
@@ -0,0 +1,22 @@
+;; See: `custom-file`.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-collection-setup-minibuffer t)
+ '(evil-want-keybinding nil)
+ '(evil-want-minibuffer t)
+ '(js-indent-level 2)
+ '(load-home-init-file t t)
+ '(package-selected-packages
+   '(lusty-explorer xah-elisp-mode lisp-extra-font-lock git-gutter helm-ag helm ivy ido-grid-mode paren-face evil-magit magit dash smart-mode-line solarized-theme fill-column-indicator undo-tree elisp-slime-nav ample-theme adaptive-wrap racer flycheck-rust company cargo))
+ '(warning-suppress-types '((undo discard-info))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -49,25 +49,10 @@
           (with-current-buffer "*scratch*"
             (emacs-lock-mode 'kill)))))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-collection-setup-minibuffer t)
- '(evil-want-keybinding nil)
- '(evil-want-minibuffer t)
- '(js-indent-level 2)
- '(load-home-init-file t t)
- '(package-selected-packages
-   '(paren-face evil-magit magit dash smart-mode-line solarized-theme fill-column-indicator undo-tree elisp-slime-nav ample-theme adaptive-wrap racer flycheck-rust company cargo))
- '(warning-suppress-types '((undo discard-info))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Custom/customize stuff is auto-edited by Emacs and superstition
+;; pressures me to have that done in its own file.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;; Package
 ;;
