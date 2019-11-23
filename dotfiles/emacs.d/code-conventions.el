7aa2b41421175d3cfb5e2b8282ac35367713344d
Stephen Bach <github@sjbach.com>
2019-11-23 17:04:36 -0500

    Emacs: prettify ^L section separators in elisp

--- dotfiles/emacs.d/code-conventions.el
+++ dotfiles/emacs.d/code-conventions.el
@@ -24,3 +24,19 @@
 (add-hook 'emacs-lisp-mode-hook #'company-mode)
 (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
 
+;; ^L is used as a section separator in GNU code. Make it look purposeful.
+(defun xah-show-formfeed-as-line ()
+  "Display the formfeed ^L char as line.
+URL `http://ergoemacs.org/emacs/emacs_form_feed_section_paging.html'
+Version 2018-08-30"
+  (interactive)
+  ;; 2016-10-11 thanks to Steve Purcell's page-break-lines.el
+  (progn
+    (when (not buffer-display-table)
+      (setq buffer-display-table (make-display-table)))
+    (aset buffer-display-table ?\^L
+          (vconcat (make-list 70 (make-glyph-code ?─ 'font-lock-comment-face))))
+    ;; (not necessary, and causes flicker)
+    ;; (redraw-frame)
+    ))
+(add-hook 'emacs-lisp-mode-hook #'xah-show-formfeed-as-line)
