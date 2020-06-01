45da64c6471fbe0bd7558ee5fb810c390516f48c
Stephen Bach <github@sjbach.com>
2020-06-01 15:36:36 -0400

    Emacs: styling, misc

--- dotfiles/emacs.d/custom.el
+++ dotfiles/emacs.d/custom.el
@@ -9,14 +9,14 @@
  '(custom-safe-themes
    '("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(evil-collection-setup-minibuffer t)
- '(evil-search-module 'evil-search)
  '(evil-symbol-word-search t)
+ '(evil-want-Y-yank-to-eol t)
  '(evil-want-keybinding nil)
  '(evil-want-minibuffer t)
  '(js-indent-level 2)
  '(load-home-init-file t t)
  '(package-selected-packages
-   '(evil-numbers vimrc-mode highlight-indent-guides evil-quickscope expand-region keyfreq lusty-explorer evil-matchit highlight-parentheses transpose-frame evil-terminal-cursor-changer smart-mode-line-powerline-theme smart-mode-line powerline paradox helpful hercules macrostep elisp-refs elisp-def hydra counsel buffer-move diminish rainbow-delimiters git-timemachine ace-window evil-surround adaptive-wrap dumb-jump which-key evil-collection latex-preview-pane lisp-extra-font-lock git-gutter helm-ag helm ivy ido-grid-mode paren-face evil-magit magit fill-column-indicator undo-tree elisp-slime-nav racer flycheck-rust company cargo))
+   '(lsp-mode nameless evil-numbers vimrc-mode highlight-indent-guides evil-quickscope expand-region keyfreq lusty-explorer evil-matchit highlight-parentheses transpose-frame evil-terminal-cursor-changer smart-mode-line-powerline-theme smart-mode-line powerline paradox helpful hercules macrostep elisp-refs elisp-def hydra counsel buffer-move diminish rainbow-delimiters git-timemachine ace-window evil-surround adaptive-wrap dumb-jump which-key evil-collection latex-preview-pane lisp-extra-font-lock git-gutter helm-ag helm ivy ido-grid-mode paren-face evil-magit magit fill-column-indicator undo-tree elisp-slime-nav racer flycheck-rust company cargo))
  '(paradox-github-token t)
  '(safe-local-variable-values '((nameless-current-name . "rm")))
  '(warning-suppress-types '((undo discard-info))))
@@ -25,5 +25,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:inherit highlight :extend t :background "color-235")))))
+ '(hl-line ((t (:inherit highlight :extend t :background "color-235"))))
+ '(nameless-face ((t (:inherit font-lock-type-face :foreground "grey50")))))
 
